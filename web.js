c9b43621a1253d8378c7194899029f17eb75f949
PARTHA KAPRI <84367590+partha-kapri@users.noreply.github.com>
2023-12-29 08:25:25 +0530

    Update web.js
    29th-dec-2nd

--- web.js
+++ web.js
@@ -1,57 +1,3 @@
-//this is my first js code
-//console.log('hello world');
-
-// let name = "partha";
-// console.log(name);
-
-// let firstName = "partha";
-// let secondName = "kapri";
-
-// let interestRate = 0.3;
-// interestRate = 1;
-// console.log(interestRate);
-
-// const newInterestRate = 0.2
-// console.log(newInterestRate);
-
-// let name = 'partha'; //string literal
-// let age = 25; //number leiteral
-// let isApproved = false; //boolean literal
-// let firstName = undefined;
-// let selectedColor = null;
-
-// let person = {
-//     name: 'partha',
-//     age: 25
-// };
-
-// //dot notation
-// person.name = 'mintu';
-
-// //bracket notation
-// person['name'] = 'new';
-
-// // console.log(person)
-// // console.log(person.name);
-
-// let selectedColors = ['red', 'blue'];
-// selectedColors[2] = 1;
-// console.log(selectedColors);
-// console.log(selectedColors.length);
-
-// //performing a task
-// function greet(firstName, lastName) {
-//     console.log('hello ' + firstName + ' ' + lastName);
-// }
-// greet('partha', 'kapri');
-// greet('mintu', 'kapri');
-
-// //calculate a value
-// function square(number) {
-//     return number * number;
-// }
-// console.log(square(3))
-
 let number1 = document.getElementById('number1')
 let number2 = document.getElementById('number2')
 let input = document.getElementById('input')
@@ -67,7 +13,6 @@ function renderRandomNumber () {
   number1.textContent = rand1
   number2.textContent = rand2
 }
-
 function checkAnswer() {
   if(answer == input.value) {
     score++
@@ -86,9 +31,7 @@ function checkAnswer() {
     showScore(score)
   }
 }
-
 function showScore(score){
   scoreoutput.textContent = score
 }
-
 renderRandomNumber()
